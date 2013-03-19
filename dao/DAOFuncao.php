0aab14733b7abfd6d50b639a46974ecce1a85fac
andregustavoo@gmail.com <>
2013-03-19 14:29:38 -0300

    Implementado os métodos gravar e localizarPorId na classe DAOFuncao

--- dao/DAOFuncao.php
+++ dao/DAOFuncao.php
@@ -3,19 +3,46 @@ require 'DAO.php';
 require  $_SERVER['DOCUMENT_ROOT'] .'/sigae/model/Funcao.php';
 class DAOFuncao {
   public function gravar(Funcao $funcao){
-      $sql="insert into funcao(descricaofuncao,setor)
+      $sql='';
+      if (isset($funcao->getId()) || $funcao->getId()==0){
+          $sql="insert into funcao(descricaofuncao,setor)
                 values(
               '".$funcao->getDescricao()."',
               '".$funcao->getSetor()."')";
+      }else{
+          $sql="update funcao set 
+              descricao='".$funcao->getDescricao()."',
+              setor='".$funcao->getSetor()."'
+                  where idfuncao=".$funcao->getId(); 
+      }
+      
       $conexao=DAO::getConexao();
       $resultado=$conexao->exec($sql);
       return $resultado;
   }
   public function excluir($idfuncao){
-      $sql="delete from funcao where id=$idfuncao";
+      $sql="delete from funcao where idfuncao=$idfuncao";
       $conexao=  DAO::getConexao();
       $resultado=$conexao->exec($sql);
       return $resultado;
+  }
+  public function localizarPorId($idfuncao){
+      $sql="select idfuncao,descricao,setor 
+          from funcao where idfuncao=$idfuncao";
+      $conexao=DAO::getConexao();
+      $tabela=$conexao->query($sql);
+      $registro=$tabela->fetch(PDO::FETCH_ASSOC);
+      if ($registro){
+          $funcao=new Funcao();
+          $funcao->setId($registro['idfuncao']);
+          $funcao->setDescricao($registro['descricao']);
+          $funcao->setSetor($registro['setor']);
+          return $funcao;
+      }else{
+          return null;
+      }
+              
+      
   }
 }
 
