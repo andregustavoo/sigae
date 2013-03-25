6f3110750ef2e24f1cd857c66d7870968cda440d
Cristiane Viana <Cristiane Viana@Cristiane>
2013-03-25 20:15:42 -0300

    Implementado a classe testegrupoatividade. Grupo: Cristiane, Monic, William e Leonardo.

---
+++ testes/testegrupoatividade.php
@@ -0,0 +1,50 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <?php
+    //Utilitário para incluir automaticamente os arquivos
+        function carregaClasse($class) {
+            $classe=$_SERVER['DOCUMENT_ROOT'] .'/sigae/model/' . $class . '.php';
+            if (file_exists($classe)){
+                require $classe;
+            }else{
+               $classe=$_SERVER['DOCUMENT_ROOT'] .'/sigae/dao/' . $class . '.php';
+               if (file_exists($classe)){
+                   require $classe;
+               }
+            } 
+       }
+       spl_autoload_register('carregaClasse');   
+    ?>
+    
+    <body>
+        <?php
+            $grupoatividade = new GrupoAtividade();
+            $grupoatividade->setDescricao('Testando');
+            $daogrupoatividade = new DAOGrupoAtividade();
+            if($daogrupoatividade->gravar($grupoatividade)){
+                echo 'Cadastro Gravado com Sucesso';
+                }
+                else{
+                    echo 'Erro Salvando Grupo Atividade';
+                }
+            $localizar = $daogrupoatividade->localizarPorId(2);
+            if($localizar){
+                echo $localizar;
+            }
+            else{
+                echo 'Erro salvando classe';
+            }
+
+        ?>
+    </body>
+</html>
+
+
